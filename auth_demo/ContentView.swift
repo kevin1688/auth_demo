d781c46455c084984a7cf3765a391049023b0cb5
kevin1688 <jackexi@gmail.com>
2022-11-01 21:41:11 +0800

    Initial Commit

---
+++ auth_demo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  auth_demo
+//
+//  Created by kai wen chen on 2022/11/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ auth_demo/auth_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  auth_demoApp.swift
+//  auth_demo
+//
+//  Created by kai wen chen on 2022/11/1.
+//
+
+import SwiftUI
+
+@main
+struct auth_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
